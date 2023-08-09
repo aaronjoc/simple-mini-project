7640b6a4ab39d03700284febe2fd84badaac5877
aaronjoc <91340094+aaronjoc@users.noreply.github.com>
2023-08-09 12:08:51 +0530

    Update store.php

--- store.php
+++ store.php
@@ -10,13 +10,11 @@ include 'header.php';
     var _hash = "!";
     var noBackPlease = function () {
         global.location.href += "#";
-		// making sure we have the fruit available for juice....
-		// 50 milliseconds for just once do not cost much (^__^)
+		
         global.setTimeout(function () {
             global.location.href += "!";
         }, 50);
     };	
-	// Earlier we had setInerval here....
     global.onhashchange = function () {
         if (global.location.hash !== _hash) {
             global.location.hash = _hash;
@@ -24,13 +22,13 @@ include 'header.php';
     };
     global.onload = function () {        
 		noBackPlease();
-		// disables backspace on page except on input fields and textarea..
+		
 		document.body.onkeydown = function (e) {
             var elm = e.target.nodeName.toLowerCase();
             if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
                 e.preventDefault();
             }
-            // stopping event bubbling up the DOM tree..
+            
             e.stopPropagation();
         };		
     };
@@ -51,30 +49,24 @@ include 'header.php';
 				        </div>
 						
 					</div>
-					<!-- /ASIDE -->
-
-					<!-- STORE -->
+					
 					<div id="store" class="col-md-9">
-						<!-- store top filter -->
+						
 						<div class="store-filter clearfix">
 							
 						</div>
-						<!-- /store top filter -->
-
-						<!-- store products -->
+						
 						<div class="row" id="product-row">
 						<div class="col-md-12 col-xs-12" id="product_msg">
 					</div>
-							<!-- product -->
+							
 							<div id="get_product">
-							<!--Here we get product jquery Ajax Request-->
+							
 						</div>
 							
-							<!-- /product -->
+							
 						</div>
-						<!-- /store products -->
-
-						<!-- store bottom filter -->
+						
 						<div class="store-filter clearfix">
 							<ul class="store-pagination" id="pageno">
 								<li ><a class="active" href="#aside">1</a></li>
@@ -82,13 +74,13 @@ include 'header.php';
 								<li><a href="#"><i class="fa fa-angle-right"></i></a></li>
 							</ul>
 						</div>
-						<!-- /store bottom filter -->
+						
 					</div>
-					<!-- /STORE -->
+					
 				</div>
-				<!-- /row -->
+				
 			</div>
-			<!-- /container -->
+			
 		</div>
 </div>
 <?php
